66766fb16aff3d76b385e7778f9434a21dae1525
Bainchiclo <baniknikinchiclo@gmail.com>
2025-11-27 14:46:23 +0800

    Update merge.py

--- merge.py
+++ merge.py
@@ -14,7 +14,7 @@ playlist_urls = [
 ] 
 
 EPG_URL = "https://github.com/Drewski2423/DrewLive/raw/refs/heads/main/DrewLive.xml.gz"
-OUTPUT_FILE = "kibz.m3u8"
+OUTPUT_FILE = "best.m3u8"
 
 def fetch_playlist(url, retries=3, timeout=30):
     headers = {"User-Agent": "Mozilla/5.0"}
