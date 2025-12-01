e0a87c272b03514bbe13008a86d93a850fed3d2d
Bainchiclo <baniknikinchiclo@gmail.com>
2025-12-01 14:38:21 +0800

    Update only.py

--- only.py
+++ only.py
@@ -5,12 +5,6 @@ from datetime import datetime
 
 playlist_urls = [
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/PH.m3u",
-    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/NBA%20HOMECOURT.m3u",
-    "https://mc-5dugfxm3jo.bunny.run/playlist.m3u8",
-    "https://mc-hda11sn95o.bunny.run/playlist.m3u8",
-    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/Roxiestreams.m3u8",
-    "http://tvpass.org/playlist/m3u",
-    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/Pixelsports.m3u8"
 ] 
 
 EPG_URL = "https://github.com/Drewski2423/DrewLive/raw/refs/heads/main/DrewLive.xml.gz"
