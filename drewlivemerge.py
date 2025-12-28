83fe64e02435ada5c0af4da000b9973c6e991ae6
Bainchiclo <baniknikinchiclo@gmail.com>
2025-12-28 09:31:56 +0800

    Update drewlivemerge.py

--- drewlivemerge.py
+++ drewlivemerge.py
@@ -9,13 +9,12 @@ playlist_urls = [
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/nbaglobe.m3u",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/IndihomeTV.m3u",
     "https://raw.githubusercontent.com/Bainchiclo/fststrm/refs/heads/main/roxstrmz.m3u8",
-    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/phtest.m3u",
     "https://raw.githubusercontent.com/doms9/iptv/refs/heads/default/M3U8/events.m3u8",
     "https://raw.githubusercontent.com/Bainchiclo/fststrm/refs/heads/main/psports.m3u8"
 ] 
 
 EPG_URL = "https://github.com/Drewski2423/DrewLive/raw/refs/heads/main/DrewLive.xml.gz"
-OUTPUT_FILE = "11.m3u"
+OUTPUT_FILE = "best.m3u8"
 
 def fetch_playlist(url, retries=3, timeout=30):
     headers = {"User-Agent": "Mozilla/5.0"}
